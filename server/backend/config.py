7522ab6b31b74e91cd7051b6f13bc4b767bb1a01
FelixTisov <fedya002.ks@gmail.com>
2023-08-08 03:14:37 +0300

    vercel.json updated

--- server/backend/config.py
+++ server/backend/config.py
@@ -5,10 +5,10 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '123456'
-    DB_HOST = os.environ.get('DB_HOST') or 'f0762448.xsph.ru'
-    DB_USER = os.environ.get('DB_USER') or 'f0762448_test_database'
-    DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'testpassword'
-    DB_NAME = os.environ.get('DB_NAME') or 'f0762448_test_database'
+    DB_HOST = os.environ.get('DB_HOST') or 'a0849712.xsph.ru'
+    DB_USER = os.environ.get('DB_USER') or 'a0849712_online-notes-editor'
+    DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'ruf2002@215'
+    DB_NAME = os.environ.get('DB_NAME') or 'a0849712_online-notes-editor'
 
 
 class DevelopementConfig(BaseConfig):
